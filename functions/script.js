f9f362c6646f368f32718069d7106b76093fe65f
DevHarsh <110157158+harshituprety@users.noreply.github.com>
2025-08-25 23:43:42 +0530

    Update script.js

--- functions/script.js
+++ functions/script.js
@@ -1,1 +1,9 @@
+// Write a function named {minutesToHours} that receives a number of minutes as parameter and returns a number representing the same amount of time but in hours.
 
+
+function minutesToHours() {
+    let minutes = parseInt(prompt("Enter number of minutes to convert"));
+    return (minutes / 60);
+}
+
+console.log(minutesToHours());
