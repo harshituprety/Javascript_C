e6b24813ee780d375a6ab972a07d4a599b490cd7
DevHarsh <110157158+harshituprety@users.noreply.github.com>
2025-08-28 00:02:04 +0530

    Update script.js

--- for-loop/script.js
+++ for-loop/script.js
@@ -1,1 +1,11 @@
+// Write a function to calculate the factorial of a number.
 
+function calculateFactorial(num) {
+    let res = 1;
+    for (let i = 1; i <= num; i++) {
+        res *= 1;
+    }
+    return res;
+}
+
+console.log(calculateFactorial(4))
