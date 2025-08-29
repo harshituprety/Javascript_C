05d9cac4cc585b460b7a9a81747138e0987bd28b
DevHarsh <110157158+harshituprety@users.noreply.github.com>
2025-08-29 23:53:21 +0530

    Update script.js

--- DOM/script.js
+++ DOM/script.js
@@ -1,1 +1,11 @@
+// Change the text content of a specific element on a web page.
 
+let b = document.getElementById("head1");
+b.textContent = "The upcoming Harry Potter TV series is currently filming in the UK, with production expected to continue until next spring.";
+
+// Change the style of an element dynamically.
+
+let beautify = document.getElementById("head2");
+beautify.style.color = "Tomato";
+beautify.style.fontStyle = "italic";
+beautify.style.backgroundColor = "Teal";
