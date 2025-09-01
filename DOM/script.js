1af02ecba0f811b608cb542937bb61c9af43fda0
DevHarsh <110157158+harshituprety@users.noreply.github.com>
2025-09-02 00:26:07 +0530

    Add files via upload

--- DOM/script.js
+++ DOM/script.js
@@ -1,33 +1,10 @@
-// Change the text content of a specific element on a web page.
+// Add a link back to the source of the text after the paragraph tag.
 
-let b = document.getElementById("head1");
-b.textContent = "The upcoming Harry Potter TV series is currently filming in the UK, with production expected to continue until next spring.";
+let link = document.createElement("a");
 
-// Change the style of an element dynamically.
+link.textContent = "Go the this link to watch";
 
-let beautify = document.getElementById("head2");
-beautify.style.color = "Tomato";
-beautify.style.fontStyle = "italic";
-beautify.style.backgroundColor = "Teal";
+link.href = "https://www.youtube.com/";
+link.target = "_blank";
 
-//Write JavaScript code to get the href attribute of an <a> element with the ID "myLink".
-
-document.getElementById("myLink").removeAttribute("href");
-
-// Change the src attribute of an <img> tag with ID "image1" to "new-image.jpg".
-
-let image = document.getElementById("image1");
-image.src = "https://variety.com/wp-content/uploads/2025/08/GettyImages-2232560579.jpg?w=1000&h=667&crop=1&resize=1360%2C907";
-
-//Clear all content inside a <section> with ID "mainSection" using innerHTML.
-
-let sec = document.getElementById("mainSection");
-sec.innerHTML = "";
-
-// Set type="button" and value="Submit" on an input element with ID "submitBtn"
-
-let type = document.getElementById("submitBtn");
-type.setAttribute("type", "button");
-
-let value = document.getElementById("submitBtn");
-value.setAttribute("value", "Submit");
+document.body.append(link);
