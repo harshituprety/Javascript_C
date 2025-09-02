8fe8807fe8e50a38e21c20ca62819e8cea726044
DevHarsh <110157158+harshituprety@users.noreply.github.com>
2025-09-03 00:14:42 +0530

    Update script.js

--- DOM/script.js
+++ DOM/script.js
@@ -9,6 +9,16 @@ link.target = "_blank";
 
 document.body.append(link);
 
+// Create a button that changes its text to "Clicked!" when you click on it.
 function change() {
   document.getElementById("check").textContent = "Clicked!";
+}
+
+// Create a custom tooltip that appears near the mouse cursor when hovering over an image.
+
+function tooltip() {
+  let hover = document.getElementById("tip");
+  hover.setAttribute("title", "Ferris wheel");
+  hover.style.width = "500px";
+  hover.style.height = "500px";
 }
