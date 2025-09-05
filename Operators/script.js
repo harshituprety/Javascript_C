f71bf96ebf870c72b6fbe77007167c9f9eeb3f6d
DevHarsh <110157158+harshituprety@users.noreply.github.com>
2025-09-05 12:57:04 +0530

    Update script.js

--- Operators/script.js
+++ Operators/script.js
@@ -1,1 +1,71 @@
+// Operators:->
+// Used to perform some operation on data
+/* Arithematic Operators
+
++, -, *, /
+Modulus
+Exponentiation
+Increment
+Decrement
+
+*/
+
+// On the basis of the number of operands they work on, operators can be classified into three types :
+
+// Unary Operators: Operators that work on single operand.
+// Example: Increment( ++) , Decrement(--)
+// Binary Operators: Operators that work on two operands.
+// Example: Addition (+), Subtraction( -) , Multiplication (*)
+// Ternary Operators: Operators that work on three operands.
+// Example: Conditional Operator( ? : )
+
+// Arithematic Operators
+
+let a = 2;
+let b = 4;
+console.log("a =", a, " & b =", b);
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+
+// Modulus: ->
+console.log("a % b =", a % b);
+// Exponentiation: ->
+console.log("a ** b =", a ** b);
+
+// Unary Opeerator:->
+let a = 2;
+let b = 4;
+
+console.log("a =", a, " & b =", b);
+
+console.log("a++ =", a++);
+console.log("a =", a);
+
+// Assignment Operators
+let a = 4;
+let b = 2;
+
+a **= 4;
+console.log("a =", a);
+
+// Comparison Operators
+let a = 6;
+let b = "5";
+
+console.log("5 == 5", a == b);
+console.log("5 != 5", a != b);
+console.log("5 > 3", a > b);
+
+// Logical Operators
+let a = 6;
+let b = 5;
+let cond1 = a > b;
+let cond2 = a === 6;
+console.log("cond1 && cond2 = ", a < b && a === 6);
+console.log("cond1 || cond2 = ", a < b || a === 6);
+console.log("cond1 != cond2 = ", a < b != a === 6);
+
+
 
