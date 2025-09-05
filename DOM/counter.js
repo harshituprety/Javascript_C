1f0cc18c429297ed2c6d63b2181476bab7102529
DevHarsh <110157158+harshituprety@users.noreply.github.com>
2025-09-05 13:01:15 +0530

    Update counter.js

--- DOM/counter.js
+++ DOM/counter.js
@@ -1,1 +1,31 @@
+let i = 0;
+
+function inBtn() {
+  i++;
+  document.getElementById("zero").innerHTML = i;
+  if (i > 0) {
+    if (i % 2 == 0) {
+      document.getElementById("zero").style.color = "red";
+    } else {
+      document.getElementById("zero").style.color = "black";
+    }
+  }
+}
+function decBtn() {
+  i--;
+  document.getElementById("zero").innerHTML = i;
+  if (i < 0) {
+    if (i % 2 == 0) {
+      document.getElementById("zero").style.color = "red";
+    } else {
+      document.getElementById("zero").style.color = "black";
+    }
+  }
+}
+
+function resBtn() {
+  i = 0;
+  document.getElementById("zero").innerHTML = i;
+  document.getElementById("zero").style.color = "black";
+}
 
