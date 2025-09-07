a22e5bf5c98efa5ffd5e47ba81d07e8425ddcce5
Thrash <110157158+harshituprety@users.noreply.github.com>
2025-09-07 12:41:27 +0530

    Update color.js

--- DOM/color.js
+++ DOM/color.js
@@ -1,5 +1,10 @@
 function fun() {
-  let a = prompt("Enter your Choice of color.");
-  document.body.style.backgroundColor = a;
+  let a = prompt("Enter your first color.");
+  let b = prompt("Enter your second color");
+  if (a && b) {
+    document.body.style.background = `linear-gradient(to right, ${a}, ${b})`;
+  }
   // document.body.innerHTML = user;
 }
+
+
