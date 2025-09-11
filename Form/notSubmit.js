a486f45b2fa9336967a3e9aa7a102717166a1ebe
Thrash <110157158+harshituprety@users.noreply.github.com>
2025-09-12 00:15:56 +0530

    Update notSubmit.js

--- Form/notSubmit.js
+++ Form/notSubmit.js
@@ -2,16 +2,24 @@ function formdata() {
   let a = document.querySelector("#name");
   let b = document.querySelector("#number");
 
-  if (a.value === "" && b.value === "") {
+  if (a.value === "") {
     a.focus();
-    b.focus();
-    alert("Please enter a value");
+    alert("Please enter a name");
     return false;
     // c == 1;
+  }
+  if (b.value.length != 10) {
+    b.focus();
+    document.querySelector("#errorage").textContent =
+      "Error: Enter a valid number";
+    document.querySelector("#errorage").style.color = "red";
+    // alert("Enter a valid number");
+    return false;
   } else {
     alert("Form submitted");
     return true;
     // c == 0;
   }
 }
+
 
