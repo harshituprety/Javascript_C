fe22782c54ec7c627483ad47cc0e63366b0d86ed
Thrash <110157158+harshituprety@users.noreply.github.com>
2025-09-17 00:12:15 +0530

    Add files via upload

---
+++ DOM/event.js
@@ -0,0 +1,12 @@
+let color1 = document.querySelector("#backOne");
+
+color1.addEventListener("click", () => {
+  color1.style.color = "yellow";
+});
+
+let bbtn = document.querySelector("#btn");
+
+bbtn.addEventListener("dblclick", () => {
+  alert("Hello");
+});
+
