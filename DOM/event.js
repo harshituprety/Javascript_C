ec1af33a09038befbcbfab2d61fd0440e2b53074
Thrash <110157158+harshituprety@users.noreply.github.com>
2025-09-17 23:37:17 +0530

    Update event.js

--- DOM/event.js
+++ DOM/event.js
@@ -10,3 +10,37 @@ bbtn.addEventListener("dblclick", () => {
   alert("Hello");
 });
 
+let over = document.querySelector("#hover");
+
+over.addEventListener("mouseover", () => {
+  over.style.backgroundColor = "chocolate";
+});
+
+over.addEventListener("mouseleave", () => {
+  over.style.backgroundColor = "white";
+});
+
+let enjoy = document.querySelector("#image");
+
+enjoy.addEventListener("mouseenter", () => {
+  enjoy.style.filter = "blur(9px)";
+});
+
+enjoy.addEventListener("mouseleave", () => {
+  enjoy.style.filter = "blur(0px)";
+});
+
+let user = document.querySelector("#formdata");
+
+user.addEventListener("submit", () => {
+  alert("Form is submitted");
+});
+
+let userName = document.querySelector("#name");
+
+userName.addEventListener("change", () => {
+  userName.style.textDecoration = "uppercase";
+});
+
+
+
