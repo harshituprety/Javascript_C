6dc467b10dea1bbab46b50bef2af335d50d67fc2
Thrash <110157158+harshituprety@users.noreply.github.com>
2025-09-20 00:00:32 +0530

    Add files via upload

---
+++ arrays/script.js
@@ -0,0 +1,45 @@
+// Declare an array named `teaFlavors` that contains the strings `"green tea`, `"black tea"`, `"oolong tea"`. Access the first element of the array and store it in a variable named `firstTea`.
+
+let teaFlavors = ["green tea", "black tea"];
+
+// let marks = [92, 83, 83, 29, 45];
+// console.log(marks);
+// console.log(marks.length);
+
+// let str = "hello";
+// console.log(str[0]);
+
+// Print all elements of array
+
+let superheroes = ["ironman", "superman", "batman", "spiderman", "aquaman"];
+//for loop
+for (let a = 0; a < superheroes.length; a++) {
+  console.log(superheroes[a]);
+}
+
+//for of
+
+let cities = ["delhi", "pune", "mumbai", "bhopal", "chennai"];
+for (let city of cities) {
+  console.log(city.toUpperCase());
+}
+
+// For a given array with marks of students -> [34,87,21,99,63,75]
+// Find the average marks of the entire class
+
+let marks = [34, 87, 21, 99, 63, 75];
+let sum = 0;
+for (let average of marks) {
+  sum += average;
+}
+console.log(sum);
+
+let avg = sum / marks.length;
+console.log(`avg marks of the class = ${avg.toFixed(2)}`);
+
+let items = [250, 645, 300, 900, 50];
+
+let i = 0;
+for (let val of items){
+  console.log(val);
+}
