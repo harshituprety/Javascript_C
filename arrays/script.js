bf94e51a0d9e351872b7b20cb51fedfe29622241
Thrash <110157158+harshituprety@users.noreply.github.com>
2025-09-20 00:25:05 +0530

    Update script.js

--- arrays/script.js
+++ arrays/script.js
@@ -37,9 +37,15 @@ console.log(sum);
 let avg = sum / marks.length;
 console.log(`avg marks of the class = ${avg.toFixed(2)}`);
 
+//For a given array with price of 5 items -> [250,645,300,900,50]
+//All items have an offer of 10% OFF on them. Change the array to store the final price after applying offer.
 let items = [250, 645, 300, 900, 50];
 
 let i = 0;
-for (let val of items){
-  console.log(val);
+for (let val of items) {
+  let offer = val / 10;
+  items[i] = items[i] - offer;
+  console.log(`value after offer = ${items[i]}`);
+  i++;
 }
+
