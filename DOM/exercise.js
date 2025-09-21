5abd15064b36345b28b93c2a5ea98b64a9bb5c9e
Thrash <110157158+harshituprety@users.noreply.github.com>
2025-09-21 16:28:52 +0530

    Update exercise.js

--- DOM/exercise.js
+++ DOM/exercise.js
@@ -14,3 +14,22 @@ for (div of divs) {
 // divs[0].innerText = "new unique value 1";
 // divs[1].innerText = "new unique value 2";
 // divs[2].innerText = "new unique value 3";
+
+//Create a new button element. Give it a text "click me", Background color of red & text color of white
+// Insert the button as the first element inside the body tag
+
+let button = document.createElement("button");
+button.innerText = "Click ME";
+
+button.style.backgroundColor = "red";
+button.style.color = "white";
+
+document.querySelector("body").prepend(button);
+
+// Create a <p> tag in html, give uit a class & some styling.
+// Now create a new class in CSS and try to append this class to the <p> element
+
+let para = document.querySelector("p");
+
+para.classList.add("newClass");
+console.log(para.classList);
