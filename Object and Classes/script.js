9b5487869d2e8722179278501dba9695239acac8
Thrash <110157158+harshituprety@users.noreply.github.com>
2025-09-29 00:10:08 +0530

    Update script.js

--- Object and Classes/script.js
+++ Object and Classes/script.js
@@ -1,1 +1,16 @@
 
+// Write a program to print the marks of a student in  an object using for loop
+// obj ={mahesh: 89, rohit:79, aavesh:58}
+let marks = {
+  mahesh: 89,
+  rohit: 79,
+  aavesh: 58,
+};
+
+for (let i = 0; i < Object.keys(marks).length; i++) {
+  console.log(Object.keys(marks)[i] + "are" + marks[Object.keys(marks)[i]]);
+}
+
+for (let keys in marks) {
+  console.log("Marks of " + keys + " are " + marks[keys]);
+}
