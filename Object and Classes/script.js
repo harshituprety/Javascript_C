cac32f7f50a3b17b06c99a1cf8cb8252f88888b5
Thrash <110157158+harshituprety@users.noreply.github.com>
2025-10-03 14:17:56 +0530

    Update script.js

--- Object and Classes/script.js
+++ Object and Classes/script.js
@@ -14,3 +14,29 @@ for (let i = 0; i < Object.keys(marks).length; i++) {
 for (let keys in marks) {
   console.log("Marks of " + keys + " are " + marks[keys]);
 }
+
+// Write a function extractNames that extracts names from an array of objects.
+
+let names = [
+  {
+    name: "Rajesh",
+    city: "Pune",
+    Contact: 8942178431,
+  },
+  {
+    name: "Prashant",
+    city: "Baroda",
+    Contact: 7236145289,
+  },
+  {
+    name: "Shridhar",
+    city: "Bhagalpur",
+    Contact: 9614236573,
+  },
+];
+
+function extractNames(arr) {
+  return arr.map((i) => i.name);
+}
+let data = extractNames(names);
+console.log(data);
