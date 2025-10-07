083e72c7b0e25a2f5267a0df5d38563e4d5f21ab
Thrash <110157158+harshituprety@users.noreply.github.com>
2025-10-07 23:24:08 +0530

    Update script.js

--- while-loops/script.js
+++ while-loops/script.js
@@ -53,3 +53,55 @@ while (i != n) {
 }
 
 console.log("You guessed it right");
+
+// Write a loop that makes seven calls to console.log to output the
+// following triangle:
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+let line = "";
+let i = 1;
+
+while (i <= 7) {
+  line += "#";
+  console.log(line);
+  i++;
+}
+
+/*Write a program that uses console.log to print all the numbers
+from 1 to 100, with two exceptions. For numbers divisible by 3, print
+"Fizz" instead of the number, and for numbers divisible by 5 (and
+not 3), print "Buzz" instead.
+
+When you have that working, modify your program to print
+"FizzBuzz" for numbers that are divisible by both 3 and 5 (and
+still print "Fizz" or "Buzz" for numbers divisible by only one of
+those).*/
+
+let a1 = 1;
+let a2 = 100;
+
+let a3 = a2 + 1;
+
+let n = 0;
+
+while (a1 < a3) {
+  if (a1 % 3 == 0 && a1 % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (a1 % 3 == 0) {
+    // n = a1;
+    console.log("Fizz");
+  } else if (a1 % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(a1);
+  }
+
+  a1++;
+}
