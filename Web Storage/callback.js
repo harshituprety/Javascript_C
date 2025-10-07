eafdfae9d284f408b0aa35ded9b082bd3f1cc6d7
Thrash <110157158+harshituprety@users.noreply.github.com>
2025-10-07 23:15:10 +0530

    Add files via upload

---
+++ Web Storage/callback.js
@@ -0,0 +1,14 @@
+console.log("Hello1");
+console.log("Hello2");
+console.log("Hello3");
+
+setTimeout(() => {
+  console.log("Good Morning");
+}, 5000);
+
+console.log("Hello4");
+console.log("Hello5");
+
+function sum(a, b) {
+  console.log(a + b);
+}
