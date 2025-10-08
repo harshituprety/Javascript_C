d9328712deb794994002c0f984eecd2cc9af8b38
Thrash <110157158+harshituprety@users.noreply.github.com>
2025-10-08 13:00:58 +0530

    Update callback.js

--- Web Storage/callback.js
+++ Web Storage/callback.js
@@ -12,3 +12,20 @@ console.log("Hello5");
 function sum(a, b) {
   console.log(a + b);
 }
+
+function getData(dataId, getNextData) {
+  setTimeout(() => {
+    console.log("data", dataId);
+    if (getNextData) {
+      getNextData();
+    }
+  }, 2000);
+}
+
+getData(1, () => {
+  getData(2, () => {
+    getData(3, () => {
+      getData(4);
+    });
+  });
+});
