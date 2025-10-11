81061e1291500191c7502805d38a9e91a5510e26
Hi-star <110157158+harshituprety@users.noreply.github.com>
2025-10-11 16:12:08 +0530

    Add files via upload

--- Pomodoro Timer/script.js
+++ Pomodoro Timer/script.js
@@ -1,1 +1,49 @@
+let startEl = document.querySelector("#start");
+let stopEl = document.querySelector("#stop");
+let resetEl = document.querySelector("#reset");
+let timerEl = document.querySelector("#timer");
 
+let count;
+let timeLeft = 10;
+
+function updateTimer() {
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
+  let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
+
+  timerEl.innerHTML = formattedTime;
+}
+
+function startTimer() {
+  count = setInterval(() => {
+    timeLeft--;
+    updateTimer();
+    if (timeLeft <= 0) {
+      clearInterval(count);
+      count = null;
+      alert("Time's Up!");
+      timeLeft = 1500;
+      updateTimer();
+    }
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(count);
+}
+
+function resetTimer() {
+  // clearInterval(count);
+  updateTimer();
+  timeLeft = 1500;
+}
+
+startEl.addEventListener("click", startTimer);
+stopEl.addEventListener("click", stopTimer);
+resetEl.addEventListener("click", resetTimer);
+
+// let x = null;
+// console.log(x);
+// console.log(typeof x);
