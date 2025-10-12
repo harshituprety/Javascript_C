176b7b8dcfbb067973d38ed795fa01f3c34c4526
Hi-star <110157158+harshituprety@users.noreply.github.com>
2025-10-12 11:22:11 +0530

    Add files via upload

--- Color Generator/script.js
+++ Color Generator/script.js
@@ -1,1 +1,29 @@
+let containerEl = document.querySelector(".container");
 
+for (let index = 0; index < 30; index++) {
+  let colorContainerEl = document.createElement("div");
+  colorContainerEl.classList.add("color-container");
+  containerEl.appendChild(colorContainerEl);
+}
+let colorContainerEls = document.querySelectorAll(".color-container");
+
+generateColor();
+
+function generateColor() {
+  colorContainerEls.forEach((colorContainerEl) => {
+    const newColor = randomColor();
+    colorContainerEl.style.backgroundColor = "#" + newColor;
+    colorContainerEl.textContent = "#" + newColor;
+  });
+}
+
+function randomColor() {
+  let ch = "0123456789abcdef";
+  let color = "";
+  let colorCodeLen = 6;
+  for (let index = 0; index < colorCodeLen; index++) {
+    const randomNumber = Math.floor(Math.random() * ch.length);
+    color += ch.substring(randomNumber, randomNumber + 1);
+  }
+  return color;
+}
