ac3619eeedf3ebf98ee49cd732d7fd8768103cde
Hi-star <110157158+harshituprety@users.noreply.github.com>
2025-10-13 15:34:15 +0530

    Add files via upload

--- Image Generator/script.js
+++ Image Generator/script.js
@@ -1,1 +1,19 @@
+let imageEl = document.querySelector(".image-container");
 
+let btnEl = document.querySelector("#btn");
+
+btnEl.addEventListener("click", () => {
+  imageSum = 10;
+  addNewImages();
+  console.log("hello");
+});
+
+function addNewImages() {
+  for (let index = 0; index < imageSum; index++) {
+    let newImgEl = document.createElement("img");
+    newImgEl.src = `https://picsum.photos/300?random=${Math.floor(
+      Math.random() * 2000
+    )}`;
+    imageEl.appendChild(newImgEl);
+  }
+}
