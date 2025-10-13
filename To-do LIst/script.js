69d19119681e6f2f4631f335c01ad75d06754308
Hi-star <110157158+harshituprety@users.noreply.github.com>
2025-10-13 15:37:32 +0530

    Add files via upload

--- To-do LIst/script.js
+++ To-do LIst/script.js
@@ -1,1 +1,34 @@
+let formEl = document.querySelector(".form");
 
+let inputEl = document.querySelector("#input");
+let ulEl = document.querySelector(".list");
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  todolist();
+});
+
+function todolist() {
+  let n = inputEl.value;
+  let liEl = document.createElement("li");
+  liEl.innerText = n;
+  ulEl.appendChild(liEl);
+  inputEl.value = "";
+  let checkBtnEl = document.createElement("div");
+  checkBtnEl.innerHTML = `
+  <i class="fa-solid fa-square-check"></i>
+  `;
+  liEl.appendChild(checkBtnEl);
+  let trashBtnEl = document.createElement("div");
+  trashBtnEl.innerHTML = `
+<i class="fa-solid fa-trash"></i>
+  `;
+  liEl.appendChild(trashBtnEl);
+
+  checkBtnEl.addEventListener("click", () => {
+    liEl.classList.toggle("checked");
+  });
+
+  trashBtnEl.addEventListener("click", () => {
+    liEl.remove();
+  });
+}
