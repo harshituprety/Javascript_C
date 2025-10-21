25be3875653d04c450c48cdafa1f90d9b1ffb936
Hi-star <110157158+harshituprety@users.noreply.github.com>
2025-10-21 23:54:58 +0530

    Add files via upload

--- Auto Text/script.js
+++ Auto Text/script.js
@@ -1,1 +1,21 @@
+let textEl = document.querySelector("#text");
+let speedEl = document.querySelector("#spd");
+let text = "Developer";
+let speed = 300 / speedEl.value;
+let i = 1;
 
+writeText();
+
+function writeText() {
+  textEl.innerText = text.slice(0, i);
+
+  i++;
+
+  if (i > text.length) {
+    i = 1;
+  }
+
+  setTimeout(writeText, speed);
+}
+
+speedEl.addEventListener("input", (e) => (speed = 300 / e.target.value));
