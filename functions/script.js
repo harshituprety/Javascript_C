0fcd3222a298c73f8edb26d0bed6e25b012ced0a
Hi-star <110157158+harshituprety@users.noreply.github.com>
2025-11-05 23:54:29 +0530

    Update script.js

--- functions/script.js
+++ functions/script.js
@@ -38,3 +38,46 @@ function fusion() {
 }
 
 fusion();
+
+/** Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+
+toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+**/
+
+var expect = (val)=>({
+  toBe(otherVal){
+    if(val===otherVal) return true;
+    else throw new Error("Not Equal");
+  },
+  notToBe(otherVal){
+    if(val!== otherVal) return true;
+    else throw new Error("Equal");
+  }  
+});
+
+expect(3).toBe(3) // true
+
+/** Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
+
+The three functions are:
+
+increment() increases the current value by 1 and then returns it.
+decrement() reduces the current value by 1 and then returns it.
+reset() sets the current value to init and then returns it.
+**/
+
+var createCounter = function(init){
+    let a = init
+    function increment(){
+        return ++a;
+    }
+    function decrement(){
+        return --a;
+    }
+    function reset(){
+        return (a = init);
+    }
+
+    return {increment,decrement,reset}
+}
